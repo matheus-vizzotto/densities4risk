18e33f30846400cbc62e3d15c325812116529ca4
Matheus Vizzotto dos Santos <matheus.vizzotto515@gmail.com>
2025-11-09 20:55:46 -0300

    organiza principais funções utilizadas

--- resources/r_translation_tests/horta_ziegelmann.r
+++ resources/r_translation_tests/horta_ziegelmann.r
@@ -10,23 +10,24 @@ L2norm = function(f, du) {sqrt(inner.product(f, f, du))}
 
 # Create rectangular matrices (3x2 and 3x4)
 matrixX <- matrix(c(1, 2,
-                   3, 4,
-                   5, 6), 
-                 nrow = 3, ncol = 2, byrow = TRUE)
+                    3, 4,
+                    5, 6), 
+                  nrow = 3, ncol = 2, byrow = TRUE)
 
 Y <- matrix(c(2, 1, 3, 4,
-                   1, 0, 2, 1,
-                   3, 2, 1, 0,
-                   1, 2, 3, 4,
-                   4, 3, 2, 1
-                   ), 
-                 nrow = 5, ncol = 4, byrow = TRUE)
+              1, 0, 2, 1,
+              3, 2, 1, 0,
+              1, 2, 3, 4,
+              4, 3, 2, 1
+), 
+nrow = 5, ncol = 4, byrow = TRUE)
 
 n = N = ncol(Y)
 p=5
 p=2
 du=0.005
-m=5001
+#m=5001
+m = nrow(Y)
 
 Ybar = rowMeans(Y)
 Ydev = Y - Ybar
@@ -87,23 +88,47 @@ gammahat = gammahat.temp
 thetahat.old = thetahat
 gammahat.old = gammahat
 
-lag_max = 3
+lag_max = 2
 bs.pvalues = vector("numeric", lag_max)
 
-# if(select_ncomp == "TRUE")
-# {
+
   bs.pvalues = vector("numeric", lag_max)
   
   # Building the estimated functions Yhat with dimension d0, d0 in {1,2,...,lag_max}.
   # See the main code for an explanation of this section.
   
-  for(d0 in 1:lag_max)
+  for(d0 in 1:(lag_max-1))
   {
+    print(d0)
     thetahatH0 = Re(thetahat.old[d0+1])
     thetahat = Re(thetahat.old[1:d0])
     gammahat = Re(gammahat.old[,1:d0])
     psihat.root = Ydev[,1:(n-p)] %*% gammahat
     psihat = matrix(0, nrow = m, ncol = d0)
-    # for(i in 1:d0) psihat[,i] = psihat.root[,i]/L2norm(psihat.root[,i], du = du); rm(i)
+    for(i in 1:d0) psihat[,i] = psihat.root[,i]/L2norm(psihat.root[,i], du = du); rm(i)
+    etahat = inner.product(psihat, Ydev, du = du)
+    Yhat = Ybar + psihat %*% etahat
+    
+    Yhat.fix = Yhat
+    Yhat.fix[Yhat.fix < 0] = 0
+    for (t in 1:N) Yhat.fix[,t] = Yhat.fix[,t]/(sum(Yhat.fix[,t])*du); rm(t)
+    
+    epsilonhat = Y - Yhat.fix
   }
-# }
+
+no_boot = B = 1000
+
+sampleCols = function(A)
+{
+  idx = sample(1:ncol(A))
+  M = matrix(0, nrow = nrow(A), ncol = ncol(A))
+  for(i in 1:length(idx))
+  {
+    M[,i] = A[,idx[i]]
+  }
+  return(M)
+}
+
+
+bs.thetahat = vector("numeric", B)
+bs = list()
