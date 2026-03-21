52d5bbe70237d32b2012a71b4dac191485a2c7af
Matheus Vizzotto dos Santos <86991045+matheus-vizzotto@users.noreply.github.com>
2026-03-21 16:34:13 -0300

    forecaster class

--- src/forecasting.py
+++ src/forecasting.py
@@ -1588,3 +1588,93 @@ def cv_dfpc_horta_zieg(
         measures.append(d1)
     
     return pd.DataFrame(measures)
+
+class DensityForecaster:
+    """
+    A pipeline for forecasting probability density functions using LQD 
+    transformation and Functional Principal Component Analysis (K-dFPC).
+
+    Attributes
+    ----------
+    model_kdfpc : object
+        The fitted K_dFPC model instance.
+    eigenvalues : numpy.ndarray
+        Eigenvalues (λ) representing variance explained by each component.
+    eigenfunctions : numpy.ndarray
+        The basis functions (φ) in the LQD space.
+    scores : numpy.ndarray
+        The expansion coefficients (η) for the training data.
+
+    Example
+    -------
+    >>> forecaster = DensityForecaster(maxlags=5)
+    >>> # Fit the model
+    >>> forecaster.fit(Y_train, Y_support_train)
+    >>> # Access stored K_dFPC attributes
+    >>> forecaster.eigenvalues
+    >>> # Generate Forecast
+    >>> supports, densities = forecaster.predict(horizon=10)
+    """
+    def __init__(self, kdfpc_kwargs=None, maxlags=10, criteria='bic'):
+        self.kdfpc_kwargs = kdfpc_kwargs or {}
+        self.maxlags = maxlags
+        self.criteria = criteria
+        self.model_lqd = None
+        self.model_kdfpc = None
+        self.model_arima = None
+
+    def fit(self, Y_train, Y_support):
+        # 1. Transform Densities
+        self.model_lqd = mLQDT(Y_train, Y_support)
+        self.model_lqd.densities_to_lqdensities(verbose=False)
+        
+        # 2. L2 Expansion (K_dFPC)
+        lqd_values = self.model_lqd.lqd.values
+        lqd_support = self.model_lqd.lqd_support
+        
+        self.kdfpc_kwargs.update({
+            "u": lqd_support,
+            "du": lqd_support[1] - lqd_support[0]
+        })
+        
+        self.model_kdfpc = K_dFPC(lqd_values)
+        self.model_kdfpc.fit(**self.kdfpc_kwargs)
+        
+        # 3. Fit ARIMA for the 'c' constant
+        self.model_arima = pm.auto_arima(
+            self.model_lqd.c, 
+            seasonal=False, 
+            information_criterion=self.criteria,
+            suppress_warnings=True
+        )
+        return self
+
+    def predict(self, horizon, var_lags=None):
+        # 1. Forecast Scores
+        k_scores = self.model_kdfpc.etahat.real.T
+        k_etahat_fc = run_forecaster(
+            k_scores, self.maxlags, self.criteria, horizon, selected_nlags=var_lags
+        )
+        
+        # 2. Forecast 'c'
+        c_forecast = self.model_arima.predict(n_periods=horizon)
+        
+        # 3. Reconstruct LQD and back to Densities
+        k_curve_forecast = self.model_kdfpc.predict(k_etahat_fc)
+        df_k_forecast = pd.DataFrame(k_curve_forecast)
+        
+        df_supports, df_densities = obtain_densities_from_lqd(
+                                        df_k_forecast,
+                                        self.model_lqd.lqd_support,
+                                        c_forecast,
+                                        t_=self.model_lqd.t,
+                                        verbose=False
+                                    )
+        
+        return {
+                "future_eigenvalues": k_etahat_fc,
+                "future_cs": c_forecast,
+                "future_supports": df_supports,
+                "futue_densities": df_densities
+        }
+    
